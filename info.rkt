58fb3352c0c14d952629dba3d3e18819c8618b9b
Metaxal <laurent.orseau@gmail.com>
2023-04-27 14:18:29 +0100

    pkg desc

--- info.rkt
+++ info.rkt
@@ -6,8 +6,8 @@
 (define build-deps '("sandbox-lib"
                      "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/used-imports.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "What bindings come from which imported module?")
+(define version "0.1")
 (define pkg-authors '(lorseau))
 (define license '(Apache-2.0 OR MIT))
 (define raco-commands
